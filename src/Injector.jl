039d721aefe178fbbe93aaba41e805cc599278f0
Nicholas Bauer <nicholasbauer@outlook.com>
2021-11-26 17:47:54 -0500

    Add Julia compat and ExprTools

--- src/Injector.jl
+++ src/Injector.jl
@@ -1,5 +1,7 @@
 module Injector
 
+using ExprTools
+
 export @inject
 
 const fdict = Dict{Symbol, Core.OpaqueClosure}()
@@ -11,55 +13,19 @@ macro inject(fname, before = nothing, after = nothing)
         fdict[fname] :
         Base.Experimental.@opaque (args...) -> f(args...)
     for m ∈ methods(f)
-        sig = m.sig
-        if m.sig isa UnionAll
-            typeparams = Symbol[]
-            while sig isa UnionAll
-                push!(typeparams, Symbol(sig.var))
-                sig = sig.body
-            end
-            println(typeparams)
-            params = sig.parameters[2:end]
-            varnames = [gensym() for i ∈ eachindex(params)]
-            call = Expr(:call, fname, [Expr(:(::), v, eval(p)) for (v, p) ∈ zip(varnames, params)]...)
-            block = Expr(:block,
-                if before !== nothing
-                    Expr(:call, before)
-                end,
-                Expr(:(=), :fbase, fbase),
-                Expr(:(=), :result, Expr(:call, :fbase, [v for v ∈ varnames]...)),
-                if after !== nothing
-                    Expr(:call, after, :result)
-                end,
-                Expr(:return, :result))
-            global aaa = (Expr(:function,
-                Expr(:where, 
-                    call,
-                    typeparams...),
-                block))
-            eval(Expr(:function,
-                Expr(:where, 
-                    call,
-                    typeparams...),
-                block))
-        else
-            params = sig.parameters[2:end]
-            varnames = [gensym() for i ∈ eachindex(params)]
-            call = Expr(:call, fname, [Expr(:(::), v, p) for (v, p) ∈ zip(varnames, params)]...)
-            block = Expr(:block,
-                if before !== nothing
-                    Expr(:call, before)
-                end,
-                Expr(:(=), :fbase, fbase),
-                Expr(:(=), :result, Expr(:call, :fbase, [v for v ∈ varnames]...)),
-                if after !== nothing
-                    Expr(:call, after, :result)
-                end,
-                Expr(:return, :result))
-            eval(Expr(:function,
-                    call,
-                    block))
-        end
+        func = signature(m)
+        varnames = [a isa Symbol ? a : a.args[1] for a ∈ func[:args]]
+        func[:body] = Expr(:block,
+            if before !== nothing
+                Expr(:call, before)
+            end,
+            #Expr(:(=), :fbase, fbase),
+            Expr(:(=), :result, Expr(:call, fbase, [v for v ∈ varnames]...)),
+            if after !== nothing
+                Expr(:call, after, :result)
+            end,
+            Expr(:return, :result))
+        eval(combinedef(func))
     end
     fdict[fname] = fbase
     nothing
